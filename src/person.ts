759c4e64b8ba8306fa15a3cb62f21734d8e6b09f
sakasanokuma <moonsunphoenix@gmail.com>
2021-09-17 17:57:01 +0900

    add person.js and person.ts

---
+++ src/person.ts
@@ -0,0 +1,11 @@
+class Person {
+    // strict modeでなぜかずっとエラーになる。
+    constructor(public age:number) {}
+    growOld = () => {
+        this.age++;
+    }
+}
+var person = new Person(1);
+setTimeout(person.growOld,1000);
+
+setTimeout(function() { console.log(person.age); },2000); // 2
